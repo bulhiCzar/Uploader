6664b7d23a49ce5858c47e89f917a5df0055bf17
bulhiCzar <70818351+bulhiCzar@users.noreply.github.com>
2020-09-18 19:17:54 +0300

    dd

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json())
 
 // app.use('/uploads', express.static(__dirname, '/uploads'));
 
-// app.use(express.static(__dirname + '/uploads'))
+app.use(express.static(__dirname + '/uploads'))
 app.use(fileUpload())
 
 
@@ -27,21 +27,21 @@ app.use('/api/file', require('./routes/file.routes'))
 console.log(path.join(__dirname, 'client', 'build'))
 
 
-app.use('/', express.static(path.join(__dirname, 'client', 'build')))
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-
-
-
-// if (process.env.NODE_ENV === 'production') {
-//     app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+// app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
 //     app.get('*', (req, res) => {
 //         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 //     })
-// }
+
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 
 
