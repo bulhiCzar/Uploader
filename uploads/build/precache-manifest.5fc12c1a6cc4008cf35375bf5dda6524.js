48620932b238100fb5af69fe25e603e962c00471
bulhiCzar <70818351+bulhiCzar@users.noreply.github.com>
2020-09-18 17:34:09 +0300

    1

---
+++ uploads/build/precache-manifest.5fc12c1a6cc4008cf35375bf5dda6524.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c526f4ded58382b4a8a9a680165a1082",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cc03bade62c33a1aa5a",
+    "url": "/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "d73081ca9b4e7b0c36e9",
+    "url": "/static/css/main.5af54775.chunk.css"
+  },
+  {
+    "revision": "0cc03bade62c33a1aa5a",
+    "url": "/static/js/2.5773e41d.chunk.js"
+  },
+  {
+    "revision": "3223ff019af3cdc1f320467fab225070",
+    "url": "/static/js/2.5773e41d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d73081ca9b4e7b0c36e9",
+    "url": "/static/js/main.a9ce0beb.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
