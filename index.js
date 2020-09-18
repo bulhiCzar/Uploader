77678f0aaa89ff1241355d9f46100fc266ee5e82
bulhiCzar <70818351+bulhiCzar@users.noreply.github.com>
2020-09-18 20:25:31 +0300

    sdfdfs

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ if (process.env.NODE_ENV === 'production') {
     
     app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
-    app.use('/static', express.static('uploads/files'))
+    app.use('/static/:id', express.static('/uploads/files'))
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
