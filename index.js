95053beae12ebfb3d8ad9e0cf7e3ce3bcf2f8a60
bulhiCzar <70818351+bulhiCzar@users.noreply.github.com>
2020-09-18 20:42:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === 'production') {
     // app.use(express.static(path.join(__dirname, 'client', 'build')))
 
     // app.use('/static', express.static(`${__dirname}/uploads/files`))
-    app.use('/static', express.static(`/uploads`))
+    app.use('/static/', express.static(`/uploads`))
     
     // app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
