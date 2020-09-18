2b08976831cf771c85933f0100f08f7053d8512b
bulhiCzar <70818351+bulhiCzar@users.noreply.github.com>
2020-09-18 19:03:26 +0300

    lk

--- index.js
+++ index.js
@@ -26,13 +26,22 @@ app.use('/api/file', require('./routes/file.routes'))
 // app.use('/api/test', require('./routes/test.routes'))
 console.log(path.join(__dirname, 'client', 'build'))
 
-if (process.env.NODE_ENV === 'production') {
-    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
-}
+
+
+
+// if (process.env.NODE_ENV === 'production') {
+//     app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//     })
+// }
 
 
 
