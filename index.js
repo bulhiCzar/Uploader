9340ef48adc2baa7048a8d4d0cc1ff404e7be628
bulhiCzar <70818351+bulhiCzar@users.noreply.github.com>
2020-09-18 20:33:28 +0300

    sd

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ if (process.env.NODE_ENV === 'production') {
 
     app.use('/static/:id', express.static('/uploads/files'))
     
-    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+    // app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
     
 
