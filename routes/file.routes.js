28339a362847bc7a1ad9f47e8f07acb0085a6ab8
bulhiCzar <70818351+bulhiCzar@users.noreply.github.com>
2020-09-18 20:09:36 +0300

    ыфв

--- routes/file.routes.js
+++ routes/file.routes.js
@@ -95,7 +95,7 @@ router.post(
                     masterLinks.unshift(hash)
                     // console.log(`../client/build/files/${filesArray[i].name}`)
 
-                    filesArray[i].mv(`client/build/files/${filesArray[i].name}`, function (err) {
+                    filesArray[i].mv(`uploads/${filesArray[i].name}`, function (err) {
                         if (err) { 
                             // console.log(err)
                             return res.status(500).json({message: 'Почему-то не загрузилось. Попробуйте еще раз', type: 'error' }) }
