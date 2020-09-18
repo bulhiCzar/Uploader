cfcaa8022af06cea457c2e0f718d515b67bbdd00
bulhiCzar <70818351+bulhiCzar@users.noreply.github.com>
2020-09-18 20:04:38 +0300

    ыва

--- index.js
+++ index.js
@@ -39,7 +39,9 @@ app.use('/api/file', require('./routes/file.routes'))
 
 if (process.env.NODE_ENV === 'production') {
     // app.use(express.static(path.join(__dirname, 'client', 'build')))
-    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+    app.use(express.static('client'));
+    // app.use(express.static('files'));
+    app.use('/', path.join(__dirname, 'client', 'build'))
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
