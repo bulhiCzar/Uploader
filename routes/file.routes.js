cbac89f5c82f1b6f87c5ceeee8e5ea545717207b
bulhiCzar <70818351+bulhiCzar@users.noreply.github.com>
2020-10-06 13:38:45 +0300

    client

--- routes/file.routes.js
+++ routes/file.routes.js
@@ -95,7 +95,7 @@ router.post(
                     masterLinks.unshift(hash)
                     // console.log(`../client/build/files/${filesArray[i].name}`)
 
-                    filesArray[i].mv(`build/static/files/${filesArray[i].name}`, function (err) {
+                    filesArray[i].mv(`client/build/static/files/${filesArray[i].name}`, function (err) {
                         if (err) { 
                             // console.log(err)
                             return res.status(500).json({message: 'Почему-то не загрузилось. Попробуйте еще раз', type: 'error' }) }
