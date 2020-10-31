b1ab01a3e4a7d3625f2d81783614c19103a24547
bulhiCzar <70818351+bulhiCzar@users.noreply.github.com>
2020-10-31 22:38:25 +0300

    d

--- index.js
+++ index.js
@@ -50,7 +50,8 @@ if (process.env.NODE_ENV === 'production') {
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
-// }else{
+}
+// else{
 //     app.use('/', express.static(`${__dirname}/client/build`))
     
 //     // app.use('/', express.static(path.join(__dirname, 'client', 'build')))
