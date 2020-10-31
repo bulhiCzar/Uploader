1234a45d6693b1f15b3be8f7030e5fa6ddfd3a33
bulhiCzar <70818351+bulhiCzar@users.noreply.github.com>
2020-10-31 23:21:13 +0300

    d

--- routes/file.routes.js
+++ routes/file.routes.js
@@ -94,7 +94,6 @@ router.post(
                     // console.log(existing)
                     fileAge = fileAge + 1
                     res.status(200).json({ message: 'Такой фаил уже был загружен ранее', type: 'warning', failLoad: fileAge })
-                    return
                 } else {
                     filesArray[i].name = hash
 
